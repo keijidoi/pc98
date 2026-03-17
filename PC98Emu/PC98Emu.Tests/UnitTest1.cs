67a20e22b50470618f73164a4730d5df4cce995c
敬司 土井 <doi.keiji.zm@gmail.com>
2026-03-17 18:04:20 +0900

    feat: initialize PC98Emu solution with console and test projects

---
+++ PC98Emu/PC98Emu.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace PC98Emu.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ PC98Emu/PC98Emu/Program.cs
@@ -0,0 +1,9 @@
+namespace PC98Emu;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("PC98Emu - PC-98 Emulator");
+    }
+}
